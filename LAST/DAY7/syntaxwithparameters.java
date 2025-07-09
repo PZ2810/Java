6318101d12124232f036a70fc7a08425fd073d55
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-07-09 22:47:12 +0530

    Add files via upload

---
+++ LAST/DAY7/syntaxwithparameters.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class syntaxwithparameters {
+    public static void printhelloworld() {
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+    }
+     public static  int calculateSum( int num1 , int num2) {
+        int sum =num1+num2;
+        return sum; 
+        
+       
+     }
+    public static void main(String[] args) {
+         Scanner sc = new Scanner (System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int sum =calculateSum(a,b);
+        System.out.println("sum is :" +sum);
+
+       
+    }
+}
