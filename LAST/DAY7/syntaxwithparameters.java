278869060c06abb6e24c05f5e912c8ee0c714ef3
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-07-10 23:33:10 +0530

    Add files via upload

--- LAST/DAY7/syntaxwithparameters.java
+++ LAST/DAY7/syntaxwithparameters.java
@@ -7,7 +7,7 @@ public class syntaxwithparameters {
         System.out.println("hello World");
         System.out.println("hello World");
     }
-     public static  int calculateSum( int num1 , int num2) {
+     public static  int calculateSum( int num1 , int num2) {//parameters or formal parameter
         int sum =num1+num2;
         return sum; 
         
@@ -17,7 +17,7 @@ public class syntaxwithparameters {
          Scanner sc = new Scanner (System.in);
         int a= sc.nextInt();
         int b=sc.nextInt();
-        int sum =calculateSum(a,b);
+        int sum =calculateSum(a,b);//argument or actual parameters
         System.out.println("sum is :" +sum);
 
        
