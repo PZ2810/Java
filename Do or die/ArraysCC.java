4b56662c2aca03551f4ff1518ffd2b1e9913391d
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-23 22:57:04 +0530

    Add files via upload

---
+++ Do or die/ArraysCC.java
@@ -0,0 +1,44 @@
+import java.util.*;
+//linear search
+public class ArraysCC {
+    // public static int linearSearch(int numbers[],int key){
+    //     for(int i =0; i<numbers.length;i++){
+    //         if(numbers[i]==key){
+    //             return i;
+    //         }
+
+    //     }
+    //     return-1;
+
+    // }
+
+    // public static void main(String[] args) {
+    //     int numbers[]={2,4,6,8,10,12,14,16};
+    //     int key =10;
+    //     int index=linearSearch(numbers, key);
+    //     if(index==-1){
+    //         System.out.println("index not found");
+    //     }else{
+    //         System.out.println("key is at index" + " "+index);
+    //     }
+    // }
+
+    //linear search
+    // public static int linearSearch(String menu[],int key){
+    //     for(String i=0;i<menu.length;i++){
+    //         if(menu[i]==key){
+
+    //         }
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     String menu[]={"dosa","samosa","bhindi","kachori","paratha"};
+    //     String key="bhindi";
+
+
+    // }
+
+    //largest Number
+    
+}
