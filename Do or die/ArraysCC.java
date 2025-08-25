05b50cd029179051bd37b487771616b51e3b7e55
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-25 23:40:25 +0530

    Add files via upload

--- Do or die/ArraysCC.java
+++ Do or die/ArraysCC.java
@@ -63,12 +63,33 @@ public class ArraysCC {
     // }
     
     //binary search
-    public static int binarysearch(int numbers[]){
-        int start =0
+    public static int binarysearch(int numbers[] ,int key){
+        int start = 0 , end= numbers.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+         if (numbers[mid] == key){
+            return mid;
+
+        }
+        if(numbers [mid]<key){
+           start=mid+1;
+        }
+        if(numbers[mid]>key){
+          end=mid-1;
+        }
+
+        }
+        return -1;
     }
     public static void main(String[] args) {
-        int number[]={2,4,6,8,10,12,14,16};
+        int numbers[]={2,4,6,8,10,12,14,16};
         int key=14;
+        int index=binarysearch(numbers, key);
+        if (index==-1){
+            System.out.println("key not found");
+        }else{
+            System.out.println("key is found at the index "+index);
+        }
 
     }
 }
