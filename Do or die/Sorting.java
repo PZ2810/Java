6797e6d85be312676d571778e88cb72d0ecd8f7b
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-09-05 21:32:23 +0530

    Add files via upload

--- Do or die/Sorting.java
+++ Do or die/Sorting.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Sorting {
     public static void bubblesort(int numbers[]) {
         for(int turns=0;turns<numbers.length-1;turns++){
@@ -12,22 +14,44 @@ public class Sorting {
 
             }
         }
-         for(int i=0;i<numbers.length;i++){
+        //  for(int i=0;i<numbers.length;i++){
+        //     System.out.print(numbers[i]+" ");
+        // }
+        // System.out.println();
+    }
+    public static void printnumbers(int numbers[]) {
+        for(int i=0;i<numbers.length;i++){
             System.out.print(numbers[i]+" ");
         }
         System.out.println();
     }
-    // public static void printnumbers(int numbers[]) {
-    //     for(int i=0;i<numbers.length;i++){
-    //         System.out.print(numbers[i]+" ");
-    //     }
-    //     System.out.println();
-    // }
+    public static void selectionsort(int numbers[]){
+        for(int i=0;i<numbers.length-1;i++){
+            int  minpos=i;
+            for(int j=i+1;j<numbers.length;j++){
+                if(numbers[j]<numbers[minpos]){
+                    minpos=j;
+
+                    
+                }
+
+            }
+             //Swap
+                    int temp= numbers[minpos];
+                    numbers[minpos]=numbers[i];
+                    numbers [i]=temp;
+
+        }
+    }
     public static void main(String[] args) {
         int numbers[]={5,4,1,3,2};
         
-        bubblesort(numbers);
-        //printnumbers(numbers);
+       // bubblesort(numbers);
+    //    selectionsort(numbers);
+    Arrays.sort(numbers,0,3);
+     printnumbers(numbers);
+   
+    
 
 
     }
